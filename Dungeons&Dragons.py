a109104215100dc0169471adf03a0f9dbc9b08cc
Salvatore Ciaramidaro <salvatore.ciaramidaro@studenti.isissgobetti.it>
2023-03-23 12:44:14 +0100

    Repo3

---
+++ Dungeons&Dragons.py
@@ -0,0 +1,28 @@
+#Ciaramidaro Salvatore
+
+#1. scrivere 10 volte la parola ciao
+#sommare i numeri da 10 a 20 (compresi gli estremi)
+#3.sommare i numeri pari fino a 30
+#4. moltiplicare i numeri da 1 a 10
+
+
+#1
+print("ciao"*10)
+
+#2
+sum = 0
+for x in range(10,21):
+ sum = sum+x
+ print(sum)
+
+#3
+
+sum1=0
+for x1 in range(0,32,2): 
+ print(sum1)
+
+#4
+mol =1
+for y in range(1,11):
+ mol1 = y*mol
+ print(mol)
